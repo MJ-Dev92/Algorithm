095dad1dc037c42378d91e650e043e4890543dcd
MinJ <77906820+MJ-Dev92@users.noreply.github.com>
2023-01-31 23:10:43 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,12 @@
+function solution(array, n) {
+    let answer = 0;
+    for (let i = 0; i < array.length; ++i) {
+        
+        if (array[i] === n) {
+            answer += 1;
+        } else {
+            answer += 0;
+        }
+    }
+    return answer;
+}
