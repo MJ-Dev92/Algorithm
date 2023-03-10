b5dc8742b273e3e3d67a4d24da51b7197fba9199
전민재 <77906820+MJ-Dev92@users.noreply.github.com>
2023-03-10 23:10:28 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,24 @@
+// 프로그래머스 숫자 문자열과 영단어
+
+let s = "one4seveneight";
+
+solution(s);
+
+function solution(s) {
+  const stringToNum = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (let i = 0; i < 10; i++) {
+    s = s.split(stringToNum[i]).join(i);
+  }
+  return (+s);
+}
