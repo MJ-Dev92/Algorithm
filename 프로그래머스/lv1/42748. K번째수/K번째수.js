c079f2cebeeb05b60dd124e66aaa03be7f98531a
전민재 <77906820+MJ-Dev92@users.noreply.github.com>
2023-04-07 21:15:50 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+  let answer = [];
+
+  for (let [i, j, k] of commands) {
+    answer.push(Number(
+      array
+        .slice(i - 1, j)
+        .sort((a, b) => a - b)[k - 1]
+    ));
+  }
+  return answer;
+}
