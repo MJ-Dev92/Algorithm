49fd24139fb223d39e8411858ac517a025f02049
전민재 <77906820+MJ-Dev92@users.noreply.github.com>
2023-04-18 19:21:13 +0900

    Update 등수 매기기.js

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -5,9 +5,7 @@ function solution(score) {
     for (let j = 0; j < score.length; j++) {
       if (score[i][0] + score[i][1] < score[j][0] + score[j][1]) {
         cnt++;
-      } else if (score[i][0] + score[i][1] === score[j][0] + score[j][1]) {
-        continue;
-      }
+      } 
     }
     answer.push(cnt);
     cnt = 1;
