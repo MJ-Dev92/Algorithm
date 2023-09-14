cb8423209713beb757c8eb8cd23356a58d286c86
전민재 <77906820+MJ-Dev92@users.noreply.github.com>
2023-09-14 23:37:54 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,15 @@
+function solution(sides) {
+    var answer = 0;
+    
+    sides.sort(function(a, b)  {
+    return a - b;
+    });
+    
+    if (sides[2] < sides[0] + sides[1]) {
+        answer = 1;
+    } else {
+        answer = 2;
+    }
+    
+    return answer;
+}
