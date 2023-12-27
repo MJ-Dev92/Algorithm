8c7796ea07480a6019f75a7b6e36e9b722dc9c7f
전민재 <77906820+MJ-Dev92@users.noreply.github.com>
2023-12-27 23:05:59 +0900

    [level 1] Title: 둘만의 암호, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,20 @@
+function solution(s, skip, index) {
+        const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    /*
+    const alphabet = []
+    for (let i = 97; i <= 122; i++) {
+        alphabet.push(String.fromCharCode(i));
+    }
+    */
+
+    skip.split('').forEach(v=>{
+        alphabet.splice(alphabet.indexOf(v),1)
+    })
+
+    const answer = s.split('').map((v)=>{
+        return alphabet[(alphabet.indexOf(v) + index) % alphabet.length]
+    }).join('')
+
+    return answer;
+}
